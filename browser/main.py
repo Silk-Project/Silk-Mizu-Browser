6453f213e3a6f02a804ce016640357fa158f6b6a
FlipArt <192363076+FlipArtYT@users.noreply.github.com>
2025-12-30 15:57:02 +0100

    Enhance URL regex to include file paths

--- browser/main.py
+++ browser/main.py
@@ -133,12 +133,16 @@ class WebEngine():
         self.page_progress.setValue(prog)
     
     def valid_url(self, url):
-        # Regex for standard http/https URLs
+        # Regex for standard http/https URLs and file paths
         regex = re.compile(
-            r'^(?:http|ftp)s?://' # http:// or https://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain...
-            r'localhost|' # localhost...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'^(?:(?:http|ftp)s?|file)://'  # file
+            r'(?:'
+                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain
+                r'localhost|' # localhost
+                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # or ip
+            r'|' # OR 
+                r'/[^\s]+' # Absolute path for file:/// schemes
+            r')'
             r'(?::\d+)?' # optional port
             r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
