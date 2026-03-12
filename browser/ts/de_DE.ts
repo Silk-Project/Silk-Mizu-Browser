e227979369f366905a88d8933191265958bd2834
FlipArtYT <192363076+FlipArtYT@users.noreply.github.com>
2026-03-12 21:07:04 +0100

    Add raw ts translation file

---
+++ browser/ts/de_DE.ts
@@ -0,0 +1,485 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AI_Extension</name>
+    <message>
+        <location filename="../main.py" line="1401" />
+        <location filename="../main.py" line="1320" />
+        <source>AI Summary</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1402" />
+        <location filename="../main.py" line="1327" />
+        <source>Summarization output will appear here...</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1403" />
+        <location filename="../main.py" line="1331" />
+        <source>Download</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1404" />
+        <location filename="../main.py" line="1336" />
+        <source>Clear</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1391" />
+        <source>Select a Directory</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>BetterWebEngine</name>
+    <message>
+        <location filename="../main.py" line="984" />
+        <source>Summarize selected text with AI</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="987" />
+        <source>Summarize page with AI</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>BrowserWindow</name>
+    <message>
+        <location filename="../main.py" line="1435" />
+        <source>Start page not found</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1435" />
+        <source>The Silk Start submodule was not found. Make sure you follow the cloning instructions carefully.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1678" />
+        <location filename="../main.py" line="1441" />
+        <source>&amp;File</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1679" />
+        <location filename="../main.py" line="1442" />
+        <source>&amp;Edit</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1680" />
+        <location filename="../main.py" line="1443" />
+        <source>&amp;View</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1681" />
+        <location filename="../main.py" line="1444" />
+        <source>&amp;Bookmarks</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1682" />
+        <location filename="../main.py" line="1445" />
+        <source>&amp;AI Summarization</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1683" />
+        <location filename="../main.py" line="1446" />
+        <source>&amp;Help</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1686" />
+        <location filename="../main.py" line="1451" />
+        <source>Program Settings</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1687" />
+        <location filename="../main.py" line="1456" />
+        <source>Quit</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1830" />
+        <location filename="../main.py" line="1690" />
+        <location filename="../main.py" line="1462" />
+        <source>New Tab</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1691" />
+        <location filename="../main.py" line="1467" />
+        <source>Back</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1692" />
+        <location filename="../main.py" line="1472" />
+        <source>Next</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1695" />
+        <location filename="../main.py" line="1478" />
+        <source>Increase page zoom by 10%</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1696" />
+        <location filename="../main.py" line="1483" />
+        <source>Decrease page zoom by 10%</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1697" />
+        <location filename="../main.py" line="1488" />
+        <source>Set page zoom to 100%</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1700" />
+        <location filename="../main.py" line="1493" />
+        <source>Manage bookmarks</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1701" />
+        <location filename="../main.py" line="1498" />
+        <source>Add current page to bookmarks</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1704" />
+        <location filename="../main.py" line="1504" />
+        <source>Toggle AI Summarization Sidebar</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1705" />
+        <location filename="../main.py" line="1509" />
+        <source>Summarize current page with AI</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1708" />
+        <location filename="../main.py" line="1515" />
+        <source>Project Page</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2273" />
+        <location filename="../main.py" line="1709" />
+        <location filename="../main.py" line="1519" />
+        <source>About</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1712" />
+        <location filename="../main.py" line="1583" />
+        <source>Go</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1852" />
+        <source>Download Request</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1853" />
+        <source>Do you really want to download</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1972" />
+        <source>Add to Bookmarks</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1978" />
+        <source>Add Current Page to Bookmarks</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1986" />
+        <source>Bookmark name: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1991" />
+        <source>Bookmark URL: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2035" />
+        <source>Settings</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2048" />
+        <source>Browser Settings</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2056" />
+        <source>Silk Start (local)</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2061" />
+        <source>URL</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2069" />
+        <source>Start page type: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2075" />
+        <source>Start page URL: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2080" />
+        <source>Search engine: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2090" />
+        <source>Theme: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2094" />
+        <source>Show bottom bar: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2098" />
+        <source>Show 'Go' button in URL bar: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2107" />
+        <source>Display warning when download is requested: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2121" />
+        <source>Program Language: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2131" />
+        <source>Default font size: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2135" />
+        <source>Javascript enabled: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2139" />
+        <source>Scrollbars enabled: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2154" />
+        <source>Install</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2262" />
+        <location filename="../main.py" line="2259" />
+        <location filename="../main.py" line="2157" />
+        <source>Model Installed</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2164" />
+        <source>Ollama not running</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2170" />
+        <source>Install AI Page Summarization Model: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2175" />
+        <source>Enable AI Page Summarization: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2178" />
+        <source>General</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2179" />
+        <source>Display</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2180" />
+        <source>Security</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2181" />
+        <source>Language</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2182" />
+        <source>Engine</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2183" />
+        <source>AI Features</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2249" />
+        <source>Installing...</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="2262" />
+        <source>The AI page summarization model has been installed successfully. You can now enable AI page summarization in the settings.</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>DownloadManager</name>
+    <message>
+        <location filename="../main.py" line="1077" />
+        <source>Downloading:</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1130" />
+        <source>Finished:</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1133" />
+        <source>Canceled:</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1137" />
+        <source>Error:</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>ExtensionItemWidget</name>
+    <message>
+        <location filename="../main.py" line="327" />
+        <source>About</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="391" />
+        <location filename="../main.py" line="376" />
+        <source>Download Request</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="377" />
+        <source>Do you really want to download</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="392" />
+        <source>Do you really want to delete</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>ManageBookmarksDialog</name>
+    <message>
+        <location filename="../main.py" line="1165" />
+        <location filename="../main.py" line="1143" />
+        <source>Manage Bookmarks</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1182" />
+        <source>Add New</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1188" />
+        <source>Delete</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1202" />
+        <source>Bookmark Name: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1206" />
+        <source>Bookmark URL: </source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="1255" />
+        <source>New Bookmark</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>WebExtensionsDialog</name>
+    <message>
+        <location filename="../main.py" line="572" />
+        <location filename="../main.py" line="560" />
+        <source>Browser Extensions</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="659" />
+        <location filename="../main.py" line="612" />
+        <source>Sort by name</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="659" />
+        <location filename="../main.py" line="612" />
+        <source>Sort by developer</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="674" />
+        <source>Installed</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="675" />
+        <source>Store</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="693" />
+        <source>No extensions found.
+Why not try to install some?</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../main.py" line="739" />
+        <source>No extensions found.</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>WebExtensionsMenu</name>
+    <message>
+        <location filename="../main.py" line="820" />
+        <source>Manage Extensions</source>
+        <translation type="unfinished" />
+    </message>
+</context></TS>
